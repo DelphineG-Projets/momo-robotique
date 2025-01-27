7112c6373fad5517d4a62c5ecdc5cef55d477a80
Delphine <112188978+DelphineG-Projets@users.noreply.github.com>
2025-01-27 13:31:57 -0500

    ajout hero et image hero

--- travauxencours/main.js
+++ travauxencours/main.js
@@ -3,3 +3,18 @@ let swiperMembre = new Swiper(".mySwiperMembre", {
     effect: "cards",
     grabCursor: true,
   });
+  
+/* EFFET PARRALAX HERO ACCUEIL*/
+document.addEventListener("mousemove", function(e) {
+  let mouseX = e.clientX;
+  let mouseY = e.clientY;
+
+  let titlesHero = document.querySelectorAll(".accueil-hero-title span");
+  titlesHero.forEach(function(title, i) {
+    let speed = (i + 1) * 0.5;
+    let offsetX = (mouseX - window.innerWidth / 2) * speed / 20;
+    let offsetY = (mouseY - window.innerHeight / 2) * speed / 20;
+    title.style.transform = `translate(${offsetX}px, ${offsetY}px)`;
+  });
+
+});
