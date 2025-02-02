b9b31a3008eb8d3ab70bb1f147b0749d135b9bc5
Delphine <delphinegrenier98@gmail.com>
2025-02-02 18:13:27 -0500

    ajout nav, ajout membres de l'équipe, ajout section vidéo

--- travauxencours/main.js
+++ travauxencours/main.js
@@ -1,8 +1,15 @@
 /* SWIPER SECTION MEMBRE*/
-let swiperMembre = new Swiper(".mySwiperMembre", {
-    effect: "cards",
-    grabCursor: true,
-  });
+var swiper = new Swiper(".mySwiperMembre", {
+  slidesPerView: "auto",
+  grabCursor: true,
+  loop: true,
+  spaceBetween: 30,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+
+});
   
 /* EFFET PARRALAX HERO ACCUEIL*/
 document.addEventListener("mousemove", function(e) {
@@ -18,3 +25,36 @@ document.addEventListener("mousemove", function(e) {
   });
 
 });
+
+/* NAVBAR : SOURCE DE https://codepen.io/munjewar/pen/jQrNWw*/ 
+(function($) { 
+  $(function() { 
+
+    //  open and close nav 
+    $('#navbar-toggle').click(function() {
+      $('nav ul').slideToggle();
+    });
+
+
+    // Hamburger toggle
+    $('#navbar-toggle').on('click', function() {
+      this.classList.toggle('active');
+    });
+
+
+    // If a link has a dropdown, add sub menu toggle.
+    $('nav ul li a:not(:only-child)').click(function(e) {
+      $(this).siblings('.navbar-dropdown').slideToggle("slow");
+
+      // Close dropdown when select another dropdown
+      $('.navbar-dropdown').not($(this).siblings()).hide("slow");
+      e.stopPropagation();
+    });
+
+
+    // Click outside the dropdown will remove the dropdown class
+    $('html').click(function() {
+      $('.navbar-dropdown').hide();
+    });
+  }); 
+})(jQuery); 
